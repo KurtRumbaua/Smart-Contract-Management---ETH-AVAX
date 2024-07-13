b2f8a63da3afd320bd11943ef441815897a3f6f2
KurtRumbaua <kurtrumbaua28@gmail.com>
2024-07-13 18:09:13 +0800

    final commit

--- contracts/Assessment.sol
+++ contracts/Assessment.sol
@@ -1,60 +1,53 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.9;
 
-//import "hardhat/console.sol";
-
 contract Assessment {
-    address payable public owner;
-    uint256 public balance;
+    address payable public admin;
+    uint256 public funds;
 
-    event Deposit(uint256 amount);
-    event Withdraw(uint256 amount);
+    event Deposited(uint256 amount);
+    event Withdrawn(uint256 amount);
+    event AdminChanged(address indexed oldAdmin, address indexed newAdmin);
 
-    constructor(uint initBalance) payable {
-        owner = payable(msg.sender);
-        balance = initBalance;
+    constructor(uint initialFunds) payable {
+        admin = payable(msg.sender);
+        funds = initialFunds;
     }
 
-    function getBalance() public view returns(uint256){
-        return balance;
+    modifier onlyAdmin() {
+        require(msg.sender == admin, "Caller is not the admin");
+        _;
     }
 
-    function deposit(uint256 _amount) public payable {
-        uint _previousBalance = balance;
-
-        // make sure this is the owner
-        require(msg.sender == owner, "You are not the owner of this account");
-
-        // perform transaction
-        balance += _amount;
-
-        // assert transaction completed successfully
-        assert(balance == _previousBalance + _amount);
-
-        // emit the event
-        emit Deposit(_amount);
+    function getFunds() public view returns(uint256) {
+        return funds;
     }
 
-    // custom error
-    error InsufficientBalance(uint256 balance, uint256 withdrawAmount);
+    function addFunds(uint256 amount) public payable onlyAdmin {
+        uint previousFunds = funds;
+        funds += amount;
+        assert(funds == previousFunds + amount);
+        emit Deposited(amount);
+    }
 
-    function withdraw(uint256 _withdrawAmount) public {
-        require(msg.sender == owner, "You are not the owner of this account");
-        uint _previousBalance = balance;
-        if (balance < _withdrawAmount) {
-            revert InsufficientBalance({
-                balance: balance,
-                withdrawAmount: _withdrawAmount
+    error InsufficientFunds(uint256 availableFunds, uint256 requestedAmount);
+
+    function removeFunds(uint256 amount) public onlyAdmin {
+        uint previousFunds = funds;
+        if (funds < amount) {
+            revert InsufficientFunds({
+                availableFunds: funds,
+                requestedAmount: amount
             });
         }
+        funds -= amount;
+        assert(funds == previousFunds - amount);
+        emit Withdrawn(amount);
+    }
 
-        // withdraw the given amount
-        balance -= _withdrawAmount;
-
-        // assert the balance is correct
-        assert(balance == (_previousBalance - _withdrawAmount));
-
-        // emit the event
-        emit Withdraw(_withdrawAmount);
+    function changeAdmin(address payable newAdmin) public onlyAdmin {
+        require(newAdmin != address(0), "New admin address is zero");
+        emit AdminChanged(admin, newAdmin);
+        admin = newAdmin;
     }
 }
--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,26 @@
-import {useState, useEffect} from "react";
-import {ethers} from "ethers";
-import atm_abi from "../artifacts/contracts/Assessment.sol/Assessment.json";
+import { useState, useEffect } from "react";
+import { ethers } from "ethers";
+import atmAbi from "../artifacts/contracts/Assessment.sol/Assessment.json";
+import { Container, Button, TextField, Typography, Box, Card, CardContent, Alert } from '@mui/material';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
+const theme = createTheme();
 
 export default function HomePage() {
   const [ethWallet, setEthWallet] = useState(undefined);
   const [account, setAccount] = useState(undefined);
   const [atm, setATM] = useState(undefined);
-  const [balance, setBalance] = useState(undefined);
+  const [funds, setFunds] = useState(undefined);
+  const [newAdmin, setNewAdmin] = useState("");
+  const [depositAmount, setDepositAmount] = useState("");
+  const [withdrawAmount, setWithdrawAmount] = useState("");
 
   const contractAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
-  const atmABI = atm_abi.abi;
+  const atmABI = atmAbi.abi;
 
   const getWallet = async() => {
     if (window.ethereum) {
@@ -17,18 +28,14 @@ export default function HomePage() {
     }
 
     if (ethWallet) {
-      const account = await ethWallet.request({method: "eth_accounts"});
+      const account = await ethWallet.request({ method: "eth_accounts" });
       handleAccount(account);
     }
   }
 
   const handleAccount = (account) => {
-    if (account) {
-      console.log ("Account connected: ", account);
-      setAccount(account);
-    }
-    else {
-      console.log("No account found");
+    if (account && account.length > 0) {
+      setAccount(account[0]);
     }
   }
 
@@ -37,11 +44,10 @@ export default function HomePage() {
       alert('MetaMask wallet is required to connect');
       return;
     }
-  
+
     const accounts = await ethWallet.request({ method: 'eth_requestAccounts' });
     handleAccount(accounts);
-    
-    // once wallet is set we can get a reference to our deployed contract
+
     getATMContract();
   };
 
@@ -49,69 +55,103 @@ export default function HomePage() {
     const provider = new ethers.providers.Web3Provider(ethWallet);
     const signer = provider.getSigner();
     const atmContract = new ethers.Contract(contractAddress, atmABI, signer);
- 
+
     setATM(atmContract);
   }
-
-  const getBalance = async() => {
+  const getFunds = async() => {
     if (atm) {
-      setBalance((await atm.getBalance()).toNumber());
+      const fundsBigNumber = await atm.getFunds();
+      setFunds(ethers.utils.formatEther(fundsBigNumber));
     }
   }
-
+  
   const deposit = async() => {
-    if (atm) {
-      let tx = await atm.deposit(1);
-      await tx.wait()
-      getBalance();
+    if (atm && depositAmount) {
+      let tx = await atm.addFunds(ethers.utils.parseEther(depositAmount));
+      await tx.wait();
+      getFunds();
+      setDepositAmount(""); // Clear the input field
     }
   }
 
   const withdraw = async() => {
-    if (atm) {
-      let tx = await atm.withdraw(1);
-      await tx.wait()
-      getBalance();
+    if (atm && withdrawAmount) {
+      let tx = await atm.removeFunds(ethers.utils.parseEther(withdrawAmount));
+      await tx.wait();
+      getFunds();
+      setWithdrawAmount(""); // Clear the input field
+    }
+  }
+
+  const changeAdmin = async() => {
+    if (atm && newAdmin) {
+      let tx = await atm.changeAdmin(newAdmin);
+      await tx.wait();
+      setNewAdmin("");
     }
   }
 
   const initUser = () => {
-    // Check to see if user has Metamask
     if (!ethWallet) {
-      return <p>Please install Metamask in order to use this ATM.</p>
+      return <Alert severity="warning">Please install MetaMask to use this wallet.</Alert>;
     }
 
-    // Check to see if user is connected. If not, connect to their account
     if (!account) {
-      return <button onClick={connectAccount}>Please connect your Metamask wallet</button>
+      return <Button variant="contained" color="primary" onClick={connectAccount}>Connect MetaMask Wallet</Button>;
     }
 
-    if (balance == undefined) {
-      getBalance();
+    if (funds === undefined) {
+      getFunds();
     }
 
     return (
-      <div>
-        <p>Your Account: {account}</p>
-        <p>Your Balance: {balance}</p>
-        <button onClick={deposit}>Deposit 1 ETH</button>
-        <button onClick={withdraw}>Withdraw 1 ETH</button>
-      </div>
-    )
+      <Card variant="outlined" sx={{ mt: 4, p: 2 }}>
+        <CardContent>
+          <Typography variant="h6">Your Account: {account}</Typography>
+          <Typography variant="h6">Your Funds: {funds} ETH</Typography>
+          <Box mt={2}>
+            <TextField
+              label="Deposit Amount (ETH)"
+              variant="outlined"
+              value={depositAmount}
+              onChange={(e) => setDepositAmount(e.target.value)}
+              fullWidth
+            />
+            <Button variant="contained" color="primary" onClick={deposit} sx={{ mt: 2 }}>Deposit ETH</Button>
+          </Box>
+          <Box mt={2}>
+            <TextField
+              label="Withdraw Amount (ETH)"
+              variant="outlined"
+              value={withdrawAmount}
+              onChange={(e) => setWithdrawAmount(e.target.value)}
+              fullWidth
+            />
+            <Button variant="contained" color="secondary" onClick={withdraw} sx={{ mt: 2 }}>Withdraw ETH</Button>
+          </Box>
+          <Box mt={2}>
+            <TextField
+              label="New Admin Address"
+              variant="outlined"
+              value={newAdmin}
+              onChange={(e) => setNewAdmin(e.target.value)}
+              fullWidth
+            />
+            <Button variant="contained" color="warning" onClick={changeAdmin} sx={{ mt: 2 }}>Change Admin</Button>
+          </Box>
+        </CardContent>
+      </Card>
+    );
   }
 
-  useEffect(() => {getWallet();}, []);
+  useEffect(() => { getWallet(); }, []);
 
   return (
-    <main className="container">
-      <header><h1>Welcome to the Metacrafters ATM!</h1></header>
-      {initUser()}
-      <style jsx>{`
-        .container {
-          text-align: center
-        }
-      `}
-      </style>
-    </main>
-  )
+    <ThemeProvider theme={theme}>
+      <Container maxWidth="sm" sx={{ mt: 4, textAlign: 'center' }}>
+        <Typography variant="h4" component="h1">Welcome to the Crypto Wallet!</Typography>
+        {initUser()}
+      </Container>
+    </ThemeProvider>
+  );
 }
